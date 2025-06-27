e444c0af64b5676b4baf0b7cbdc88cb40c9f368b
kurtcodina <128434639+kurtcodina@users.noreply.github.com>
2025-06-27 21:06:22 +0800

    Initial Commit

---
+++ SimplePluginManager/SimplePluginManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePluginManagerApp.swift
+//  SimplePluginManager
+//
+//  Created by codina on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct SimplePluginManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
